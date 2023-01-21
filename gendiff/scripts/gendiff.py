bbef76a4fde6172e7f46781edcf7e40da716cf5e
Vitaliy Vorobyov <vitalik.w555@gmail.com>
2023-01-21 12:37:01 +0300

    Fixed lint issues

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,7 +10,12 @@ def main():
     parser.add_argument("first_file")
     parser.add_argument("second_file")
 
-    args = parser.parse_args()
+    parser.add_argument("-f",
+                        "--format",
+                        metavar="FORMAT",
+                        help="set format of output")
+
+    parser.parse_args()
 
 
 if __name__ == "__main__":
