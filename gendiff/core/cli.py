7a359ccccb0816ef9f37bc4c62d20a3f7d769036
v3code <vitalik.w555@gmail.com>
2023-06-30 15:30:11 +0300

    refactored cli

---
+++ gendiff/core/cli.py
@@ -0,0 +1,15 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="gendiff",
+    description="Compares two configuration files and shows a difference."
+)
+
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+
+parser.add_argument("-f",
+                    "--format",
+                    required=True,
+                    metavar="FORMAT",
+                    help="set format of output")
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,22 +1,10 @@
 import argparse
 
+from gendiff.core.cli import parser
 from gendiff.core.gendiff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog="gendiff",
-        description="Compares two configuration files and shows a difference."
-    )
-
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-
-    parser.add_argument("-f",
-                        "--format",
-                        metavar="FORMAT",
-                        help="set format of output")
-
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
