bf5ec3a4d67385731a553d0e23f2c4c9e4529a25
v3code <vitalik.w555@gmail.com>
2023-07-01 00:17:39 +0300

    fixed some issues with tests

--- gendiff/core/generator.py
+++ gendiff/core/generator.py
@@ -1,9 +1,9 @@
 from gendiff.core.diff_descriptors import build_diff_descriptors
 from gendiff.core.parser import load_file
-from gendiff.formats import get_renderer, JSON
+from gendiff.formats import get_renderer, STYLISH
 
 
-def generate_diff(file_path1, file_path2, render_format=JSON):
+def generate_diff(file_path1, file_path2, render_format=STYLISH):
     file_1 = load_file(file_path1)
     file_2 = load_file(file_path2)
     diff = build_diff_descriptors(file_1, file_2)
--- gendiff/formats/json.py
+++ gendiff/formats/json.py
@@ -1,7 +1,7 @@
 import json
 
-INDENT = 2
+from gendiff.core.config import JSON_INDENT
 
 
 def render_diff_json(descriptors):
-    json.dumps(descriptors, indent=INDENT)
+    json.dumps(descriptors, indent=JSON_INDENT)
