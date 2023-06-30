e43892ee26fefa8707cebee565fbc384f94f2c1f
v3code <vitalik.w555@gmail.com>
2023-07-01 01:58:41 +0300

    Fixed fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,56 @@
 import pytest
-from fixtures import *
+
+from gendiff.core.diff_descriptors import DELETED, UNCHANGED, ADDED, MODIFIED
+
+
+@pytest.fixture
+def flat_files():
+    file_1 = {
+        'host': 'hexlet.io',
+        'timeout': 50,
+        'proxy': '123.234.53.22',
+        'follow': False
+    }
+
+    file_2 = {
+        'timeout': 20,
+        'verbose': True,
+        'host': 'hexlet.io',
+    }
+
+    return file_1, file_2
+
+
+@pytest.fixture
+def flat_descriptors():
+    return [
+        {
+            'key': 'follow',
+            'value': False,
+            'type': DELETED
+        },
+        {
+            'key': 'host',
+            'value': 'hexlet.io',
+            'type': UNCHANGED
+        },
+        {
+            'key': 'proxy',
+            'value': '123.234.53.22',
+            'type': DELETED
+        },
+        {
+            'key': 'timeout',
+            'value':
+                {
+                    'before': 50,
+                    'after': 20
+                },
+            'type': MODIFIED
+        },
+        {
+            'key': 'verbose',
+            'value': True,
+            'type': ADDED
+        }
+    ]
